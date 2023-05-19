a3e60d54a41c10c3d7d1f1282633d0cfe69c97da
Oleshkooo <olehchoma@gmail.com>
2023-05-20 02:42:06 +0300

    implement auth

--- test/src/main.tsx
+++ test/src/main.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.scss";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.scss'
+
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
