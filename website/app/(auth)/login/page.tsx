a08453824b096454c552b135816206d000b3ef5f
NazarHolub <webblgch@gmail.com>
2023-05-28 06:28:34 +0300

    added register and login pages"

--- website/app/(auth)/login/page.tsx
+++ website/app/(auth)/login/page.tsx
@@ -1,20 +1,88 @@
+'use client'
+
 import { type NextPage } from 'next'
 import Link from 'next/link'
+import { Icons } from '@/components/ui/Icons'
+import { Input } from '@/components/ui/Input'
+import { Button } from '@/components/ui/Button'
+import { toast } from '@/hooks/use-toast'
 
-const Login: NextPage = () => {
+import { PostReqBody } from '@/app/api/login/route'
+import { Fetch } from '@/utils/Fetch'
+import { signIn } from 'next-auth/react'
+import { stringify } from 'querystring'
+
+const Register: NextPage = () => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const email = e.currentTarget.elements[0].value;
+        const pass = e.currentTarget.elements[1].value;
+
+        if (!email || !pass) {
+            toast({
+                title: 'Error',
+                description: 'Please fill out email and password', variant: 'destructive'
+            });
+            return;
+        }
+        const creds = JSON.stringify({
+            email: email,
+            password: pass
+        })
+        const res = await Fetch('/api/login', {
+            body: {
+                creds
+            }
+        })
+
+        void signIn(
+            "credentials",
+            {
+                username: email,
+                password: pass,
+                redirect: true,
+                dashboard: '/dashboard/recieve'
+            }
+        )
+
+        toast({
+            title: 'Success',
+            description: 'Successfully logged in!', variant: 'default'
+        });
+
+        // TODO redirect
+    }
+
     return (
-        <main>
-            <Link href="/">Back</Link>
-            <div>
-                <h2>Welcome back!</h2>
-                <form action="">
-                    <input name="email" type="email" placeholder="Enter your email" />
-                    <input name="password" type="password" placeholder="Enter your password" />
-                    <button type="submit">Sign in</button>
+        <div className="container flex h-screen w-full flex-col gap-[50px]">
+            <Link href="/" className='flex items-center hover:text-gray-400 transition justify-start'>
+                <Icons.chevronLeft className="mr-2 h-4 w-4" />
+                Back
+            </Link>
+            <div className="mx-auto flex w-full flex-col items-center justify-center space-y-6 sm:w-[350px]">
+                <div className="flex flex-col space-y-2 text-center">
+                    <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
+                    <p className="text-sm text-muted-foreground">
+                        Enter your email and password to sign in to your account
+                    </p>
+                </div>
+                <form className='w-full flex flex-col gap-4' onSubmit={handleSubmit}>
+                    <Input type='text' name='email' placeholder='name@example.com' />
+                    <Input type='password' name='password' placeholder='Your password' />
+                    <Button type='submit'>Sign in</Button>
                 </form>
+                <p className="px-8 text-center text-sm text-muted-foreground">
+                    <Link
+                        href="/register"
+                        className="hover:text-brand underline underline-offset-4 hover:text-gray-400 transition"
+                    >
+                        Don't have an account? Sign Up
+                    </Link>
+                </p>
             </div>
-        </main>
+        </div>
     )
 }
 
-export default Login
+export default Register
--- website/app/(auth)/register/page.tsx
+++ website/app/(auth)/register/page.tsx
@@ -1,30 +1,87 @@
+'use client'
+
 import { type NextPage } from 'next'
 import Link from 'next/link'
+import { Icons } from '@/components/ui/Icons'
+import { Input } from '@/components/ui/Input'
+import { Button } from '@/components/ui/Button'
+import { toast } from '@/hooks/use-toast'
 
 const Register: NextPage = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const email = e.currentTarget.elements[0].value;
+        const pass = e.currentTarget.elements[1].value;
+        const cpass = e.currentTarget.elements[2].value;
+
+        if (!email || !pass || !cpass) {
+            toast({
+                title: 'Error',
+                description: 'Please fill out all the fields', variant: 'destructive'
+            });
+            return;
+        }
+
+        if (pass !== cpass) {
+            toast({
+                title: 'Error',
+                description: 'Passwords don\'t match', variant: 'destructive'
+            });
+            return;
+        }
+
+        if (!email.includes('@')) {
+            toast({
+                title: 'Error',
+                description: 'Please provide a valid email', variant: 'destructive'
+            });
+            return;
+        }
+
+        if (pass.length < 8) {
+            toast({
+                title: 'Error',
+                description: 'Password is too short (8 symbols minimum)', variant: 'destructive'
+            });
+            return;
+        }
+
+        // TODO register fetch
+
+        toast({
+            title: 'Success',
+            description: 'Successfully signed up!', variant: 'default'
+        });
+
+        // TODO redirect
+    }
+
     return (
-        <div className="container flex h-screen w-screen flex-col items-center justify-center">
-            <Link href="/" className="absolute left-4 top-4 md:left-8 md:top-8">
-                <>
-                    {/* <Icons.chevronLeft className="mr-2 h-4 w-4" /> */}
-                    Back
-                </>
+        <div className="container flex h-screen w-full flex-col gap-[50px]">
+            <Link href="/" className='flex items-center hover:text-gray-400 transition justify-start'>
+                <Icons.chevronLeft className="mr-2 h-4 w-4" />
+                Back
             </Link>
-            <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="mx-auto flex w-full flex-col items-center justify-center space-y-6 sm:w-[350px]">
                 <div className="flex flex-col space-y-2 text-center">
-                    {/* <Icons.logo className="mx-auto h-6 w-6" /> */}
                     <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
                     <p className="text-sm text-muted-foreground">
-                        Enter your email to sign in to your account
+                        Enter your email and password to sign in to your account
                     </p>
                 </div>
-                {/* <UserAuthForm /> */}
+                <form className='w-full flex flex-col gap-4' onSubmit={handleSubmit}>
+                    <Input type='text' name='email' placeholder='name@example.com' />
+                    <Input type='password' name='password' placeholder='Your password' />
+                    <Input type='cpassword' name='password' placeholder='Confirm password' />
+                    <Button type='submit'>Sign up</Button>
+                </form>
                 <p className="px-8 text-center text-sm text-muted-foreground">
                     <Link
-                        href="/register"
-                        className="hover:text-brand underline underline-offset-4"
+                        href="/login"
+                        className="hover:text-brand underline underline-offset-4 hover:text-gray-400 transition"
                     >
-                        Don't have an account? Sign Up
+                        Already have an account? Sign in
                     </Link>
                 </p>
             </div>
